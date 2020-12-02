51de8acd8abcf0c136f1858c6789e9ea5b77c4a1
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-12-02 08:07:33 +0530

    Fix for Latest Update
    Fix for Latest Update

--- autobuyer.js
+++ autobuyer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FUT 21 Autobuyer Menu with TamperMonkey
 // @namespace    http://tampermonkey.net/
-// @version      2.0.2
+// @version      2.0.3
 // @updateURL    https://github.com/chithakumar13/Fifa-AutoBuyer/blob/master/autobuyer.js
 // @description  FUT Snipping Tool
 // @author       CK Algos

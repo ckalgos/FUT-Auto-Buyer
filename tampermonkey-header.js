33aa8c6944b2979339ff979abedc24fe3bfeea2a
Ck Algos <chithakumar13@gmail.com>
2021-09-11 01:39:18 +0530

    Version Update

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,7 +2,7 @@ module.exports = {
   headers: {
     name: "FUT Auto Buyer",
     namespace: "http://tampermonkey.net/",
-    version: "1.0.0",
+    version: "1.0.1",
     description: "FUT Auto Buyer",
     author: "CK Algos",
     match: [

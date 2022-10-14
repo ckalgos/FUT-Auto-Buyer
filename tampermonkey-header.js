b68d1c3d4ea78a5b08622bc62b69128464a8a800
Ck Algos <chithakumar13@gmail.com>
2022-10-14 09:55:47 +0200

    fix sbcs enhancer

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,7 +2,7 @@ module.exports = {
   headers: {
     name: "FUT Auto Buyer",
     namespace: "http://tampermonkey.net/",
-    version: "1.4.3",
+    version: "1.4.4",
     description: "FUT Auto Buyer",
     author: "CK Algos",
     match: [
